975e1b80eb5b2eafed5682bdc3b24f7f4579d962
Paola Urdiales <Al03107338@tecmilenio.mx>
2025-02-16 10:13:31 -0600

    Se hicieron actualizaciones en los html, para que desde render se pudieran ingresar a las páginas

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const path = require("path");
 
-
 dotenv.config(); 
 
 const app = express();
@@ -13,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json()); 
 app.use(cors()); 
 
-//CONEXIÓN A MONGO
+// CONEXIÓN A MONGO
 if (process.env.NODE_ENV !== 'test') { 
     console.log('Conexión a mongo en:', process.env.MONGO_URI);
 }
@@ -26,27 +25,40 @@ mongoose.connect(process.env.MONGO_URI)
     })
     .catch(err => console.error('Error al conectar a MongoDB', err));
 
-//importacion de rutas
+// Importación de rutas
 const autenticacion = require('./routes/rutaAutenticacion');
 const rutasCarros = require('./routes/rutasCarros');
 
 app.use('/act4', autenticacion);
 app.use('/act4', rutasCarros);
 
+// Servir archivos estáticos desde la carpeta 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/',(req,res)=>{ // ruta principal de la aplicació
-    res.send('Hola estas en mi API');
-})
+// Rutas para servir los archivos HTML sin necesidad de agregar '.html'
+app.get("/registro", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "registro.html"));
+});
 
-//para que la aplicación comience a funcionar
+app.get("/automoviles", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "automoviles.html"));
+});
+
+// Ruta principal de la aplicación
+app.get('/', (req, res) => {
+    res.send('Hola, estas en mi API');
+});
+
+// Para que la aplicación comience a funcionar
 if (process.env.NODE_ENV !== 'test') {
     app.listen(PORT, () => {
         console.log(`Servidor corriendo en: http://localhost:${PORT}`);
     });
 }
 
+// Manejo de rutas no encontradas
 app.use((req, res, next) => {
     res.status(404).json({ message: "La ruta que buscas no existe" });
 });
+
 module.exports = app;
